7fa26632f9b4e7912040dab862d1b678cde92bea
Bojan Matić <bojan.matic@tradecore.com>
2015-06-28 13:14:33 +0200

    Remove cruft from webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,7 @@ module.exports = {
     loaders: [
       {
         test: /\.jsx?$/,
-        // loader: ['react-hot', 'jsx', 'babel'],
-        // loader: ['jsx', 'babel'],
-        // loader: ['babel'],
-        loader: ['jsx'],
+        loader: ['react-hot', 'jsx', 'babel'],
         exclude: /node_modules/
       }
     ]
@@ -14,15 +11,12 @@ module.exports = {
   externals: {
     //don't bundle the 'react' npm package with our bundle.js
     //but get it from a global 'React' variable
-    'react': 'React'
+    // 'react': 'React'
   },
   entry: {
     helloWorld: './src/main.js'
-    // helloWorld: './src/main.jsx'
   },
   output: {
-    // filename: 'dist/[name].js'
-    // filename: 'dist/bundle.js'
-    filename: 'bundle.js'
+    filename: 'dist/webpack-bundle.js'
   },
 };
